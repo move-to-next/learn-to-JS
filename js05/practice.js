5275bd5497fd7a5c5addea135594948222e32ef2
kim jae eun <rlawodms95@naver.com>
2025-01-22 23:55:51 +0900

    javascript - 배열의 메서드 복습습

---
+++ js05/practice.js
@@ -0,0 +1,12 @@
+document.write("slice 활용"+"<br>");
+let a = [1,2,3,4,5];
+document.write("0번 인덱스에서부터 3번인덱스 앞까지 자르기"+a.slice(0,3)+"<br>");
+
+document.write("foreach 활용"+"<br>");
+let b = ["서울","경기","부산","전주","울산"];
+b.forEach(function(item,index){
+    document.write(index+" "+item+"<br>");
+    a[index]=index;
+})
+
+document.write("reduce 활용"+)
