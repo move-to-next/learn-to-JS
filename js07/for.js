3102d3cbd1b391309f884879304f2b22665892f2
kim jae eun <rlawodms95@naver.com>
2025-01-28 23:37:31 +0900

    javascript - for문 활용해서 문제풀이이

---
+++ js07/for.js
@@ -0,0 +1,24 @@
+for(let i = 0 ; i <= 5 ; i++){
+    console.log(i);
+}
+
+document.write("--문제--"+"<br>");
+document.write("주어진 숫자 배열에서 가장 큰 수와 가장 작은 수를 찾아 출력하는 프로그램을 작성해보세요."+"<br>");
+document.write("const numbers = [18, 5, 23, 12, 41, 3, 28, 35];"+"<br>");
+
+const numbers = [18, 5, 23, 12, 41, 3, 28, 35];
+let big = numbers[0];
+let small = numbers[0];
+
+for(let i = 1; i < numbers.length; i++){
+    if(numbers[i] > big){
+        big = numbers[1];
+    };
+
+    if(numbers[i] < small){
+        small = numbers[1];
+    };
+};
+
+document.write("가장 큰 수: " + big);
+document.write("가장 작은 수: " + small);
