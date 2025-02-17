82f9cc8c6bad99c064e1b0054566febd66f54b60
kim jae eun <rlawodms95@naver.com>
2025-02-18 06:01:44 +0900

    계산기 - javascript 파일 생성 후 DOM 객체 불러오기

---
+++ calculator/calculator.js
@@ -0,0 +1,31 @@
+// DOM 불러오기
+const calText = document.querySelector(".cal-text"); // 계산기 화면
+const btn = document.querySelectorAll("button"); // 계산기 버튼
+
+const no1 = document.querySelector(".no1"); // 버튼 1
+const no2 = document.querySelector(".no2"); // 버튼 2
+const no3 = document.querySelector(".no3"); // 버튼 3
+const no4 = document.querySelector(".no4"); // 버튼 4
+const no5 = document.querySelector(".no5"); // 버튼 5
+const no6 = document.querySelector(".no6"); // 버튼 6
+const no7 = document.querySelector(".no7"); // 버튼 7
+const no8 = document.querySelector(".no8"); // 버튼 8
+const no9 = document.querySelector(".no9"); // 버튼 9
+
+const plus = document.querySelector(".plus"); // 버튼 +
+const minus = document.querySelector(".minus"); // 버튼 -
+const multiply = document.querySelector(".multiply"); // 버튼 ×
+const division = document.querySelector(".division"); // 버튼 ÷
+const equal = document.querySelector(".equal"); // 버튼 =
+
+// 코드 입력
+// no1.addEventListener("click", () => {
+    // calText.textContent = "1";
+// })
+// no2.addEventListener("click", () => {
+    // calText.textContent = "2";
+// })
+// 
+// if(calText.textContent){
+    // for(let f = 0, f < 9, f++){}
+// }
