4d05a1aa6d219eb4eba717b7a193ad46d6c1ed42
kim jae eun <rlawodms95@naver.com>
2025-02-26 13:17:08 +0900

    javascript - for...in문 복습 및 for...in문 활용한 간단한 문제풀이

---
+++ JS-practice/for03/forin.js
@@ -0,0 +1,36 @@
+// 객체의 for...in문
+
+const animal = {
+    kind: 'poppy',
+    name: '하리',
+    age: 7,
+    color:'ivory'
+} // 객체 animal
+
+for (let poppy in animal){
+    console.log(`${poppy}: ${animal[poppy]}`);
+} // animal 이라는 객체에 접근해 객체내부 요소의 키와 값으로 반환한다.
+
+
+
+const scores = {
+    철수: 85,
+    영희: 92,
+    민수: 78,
+    수진: 95,
+    지훈: 88
+};
+
+// 모든 학생의 평균 점수 구하기
+let totalScore = 0; // 학생들의 총 점수
+let studentCount = 0; // 학생 수
+
+for (let student in scores){
+    totalScore += scores[student]; // 학생들의 총 점수 누적
+    studentCount++; // scores 객체 순회하면서 학생수 구하기
+};
+
+const average = totalScore / studentCount; // 평균값 변수 만들고 총 점수 / 학생 수
+console.log(average);
+
+// 
