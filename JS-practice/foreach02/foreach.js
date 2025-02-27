fb89e6c943edc0569dbae84956e2b183a65dd80d
kim jae eun <rlawodms95@naver.com>
2025-02-27 16:24:00 +0900

    javascript - foreach복습 및 foreach를 활용한 간단한 문제풀이

--- JS-practice/foreach02/foreach.js
+++ JS-practice/foreach02/foreach.js
@@ -10,7 +10,6 @@ const products = [
 
 // foreach를 사용해서 모든 전자제품의 이름과 가격을 "상품명: 가격원" 형식으로 콘솔에 출력하세요.
 
-
 // let sum = {};
 
 // for (let electric of products){
@@ -32,4 +31,28 @@ products.forEach(product => {
         console.log(`상품명: ${product.name}, 가격: ${product.price}원`);
     }
 });
-// 전자제품의 이름과 가격 출력
+
+// foreach를 사용해서 모든상품의 가격을 누적해서 총 가격을 계산하기
+
+let totalPrice = 0; // 초기값을 만든다.
+products.forEach(product => { // products 배열을 순회해서
+  totalPrice += product.price;  // products의 요소인 product(매개변수)의 price를 totalPrice에 누적시킨다.
+});
+
+console.log(`총 갸격: ${totalPrice}원`);
+
+// 가구 카테고리 상품들의 평균가격을 계산하기
+let furnitureTotal = 0;
+let furnitureCount = 0;
+
+products.forEach(product => {
+  if(product.category === "가구"){
+    furnitureTotal += product.price;
+    furnitureCount++                // if문으로 해봤을때 category가 가구인게 3개니까 3번 더함.
+  };
+})
+const furnitureAvg = furnitureCount > 0 ? Math.round((furnitureTotal / furnitureCount) / 100) * 100 : 0;
+// 가구상품이 없으면 0이 나오도록 삼항연산자 사용.
+// 만약에 furnitureCount가 0이면 0으로 나누는 대신 자바스크립트에서는 infinity라는 특수값이 반환된다.
+// 예상치 못한 값이 반환될 수 있으므로 이런 방어적 프로그래밍이 필요하다.
+console.log(furnitureAvg);
